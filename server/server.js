9a80dc3d109c3488b506ce5338209e4c28e44e88
Susanta <inbox.susanta@gmail.com>
2018-08-18 20:21:20 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,24 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New user connected");
 
-    // socket.emit('newEmail', {
-    //     from: 'example@example.com',
-    //     text: 'Hello socket',
-    //     createdAt: 1232
-    // });
-
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'see ya',
-        createdAt: 1232
-    });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
